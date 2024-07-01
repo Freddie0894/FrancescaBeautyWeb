d0155b28ab979c8805324a2a439b6462c8ee0fe5
Freddie0894 <freddie0894@gmail.com>
2024-07-01 15:18:30 +0200

    update email send functions

--- app/static/formControl.js
+++ app/static/formControl.js
@@ -6,6 +6,10 @@ function validateEmail(email) {
 document.getElementById("send-verification-code").addEventListener('click', function() {
     const emailInput = document.getElementById('email');
     const emailError = document.getElementById('email-error');
+    const codeSentMessage = document.getElementById('code-sent-message');
+    const resendSection = document.getElementById('resend-section');
+    const resendTimer = document.getElementById('resend-timer');
+    const emailVerified = document.getElementById('email-verified');
 
     console.log('Send Verification Code button clicked');
     console.log('Email input value:', emailInput.value);
@@ -24,15 +28,33 @@ document.getElementById("send-verification-code").addEventListener('click', func
             if(response.ok) {
                 console.log('Verification code sent successfully');
                 document.getElementById('verification-section').style.display = 'block';
+                codeSentMessage.style.display = 'block';
+                resendSection.classList.remove('enabled');
+
+                // contatore 30s
+                let timer = 30;
+                resendTimer.textContent = timer;
+                const interval = setInterval(() => {
+                    timer--;
+                    resendTimer.textContent = timer;
+                    if (timer <= 0) {
+                        clearInterval(interval);
+                        resendSection.classList.add('enabled');
+                        resendSection.style.cursor = 'pointer';
+                    }
+                }, 1000);
             } else {
                 alert('Errore nell\'invio del codice di verifica. Riprova più tardi.');
             }
+        }).catch(error => {
+            console.error('Error:', error);
         });
     }
 });
 
 document.getElementById("verify-code").addEventListener('click', function() {
     const verificationCodeInput = document.getElementById('verification-code');
+    const emailVerified = document.getElementById('email-verified');
 
     console.log('Verify Code button clicked');
     console.log('Verification code input value:', verificationCodeInput.value);
@@ -47,10 +69,59 @@ document.getElementById("verify-code").addEventListener('click', function() {
         if(response.ok) {
             console.log('Verification successful');
             document.getElementById('confirm-booking').disabled = false;
-            alert('Email verificata con successo.');
+            document.getElementById('send-verification-code').style.display = 'none';
+            document.getElementById('verification-section').style.display = 'none';
+            emailVerified.style.display = 'inline';
         } else {
             console.log('Verification failed');
             alert('Codice di verifica non valido. Riprova.');
+            emailVerified.style.display = 'none';
         }
+    }).catch(error => {
+        console.error('Error:', error);
     });
+});
+
+
+// ripetuto per pulsante di re-invio codice verifica
+document.getElementById('resend-section').addEventListener('click', function() {
+    if (this.classList.contains('enabled')) {
+        const emailInput = document.getElementById('email');
+        const emailError = document.getElementById('email-error');
+        const resendTimer = document.getElementById('resend-timer');
+
+        if (validateEmail(emailInput.value)) {
+            emailError.classList.remove('visible');
+            fetch('/send_verification_code', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: emailInput.value })
+            }).then(response => {
+                if (response.ok) {
+                    console.log('Verification code sent successfully');
+                    this.classList.remove('enabled');
+
+                    // Inizia un nuovo contatore di 30 secondi
+                    let timer = 30;
+                    resendTimer.textContent = timer;
+                    const interval = setInterval(() => {
+                        timer--;
+                        resendTimer.textContent = timer;
+                        if (timer <= 0) {
+                            clearInterval(interval);
+                            this.classList.add('enabled');
+                        }
+                    }, 1000);
+                } else {
+                    alert('Errore nell\'invio del codice di verifica. Riprova più tardi.');
+                }
+            }).catch(error => {
+                console.error('Error:', error);
+            });
+        } else {
+            emailError.classList.add('visible');
+        }
+    }
 });
